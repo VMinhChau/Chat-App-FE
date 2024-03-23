a3dc10516054b627e4062a5d800c9a08df46c343
ANH QUAN <quannda@a-livesystems.com>
2024-03-23 18:41:42 +0700

    Update auth page

--- src/pages/auth/Top.js
+++ src/pages/auth/Top.js
@@ -1,8 +1,9 @@
 import { Link as RouterLink } from "react-router-dom";
 // sections
-import { Stack, Typography, Link, Button, Box } from "@mui/material";
+import { Stack, Typography, Link, Button, Box, css } from "@mui/material";
 import SignUpIcon from "../../assets/images/auth/signup_icon.svg";
 import SignInIcon from "../../assets/images/auth/signin_icon.svg";
+import csscommon from "../../css/authCommon.module.css";
 
 // ----------------------------------------------------------------------
 
@@ -10,38 +11,30 @@ export default function TopPage() {
   return (
     <>
       <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
-        <Typography variant="h4" style={{ textAlign: "center" }}>
-          My Crystal
-        </Typography>
+      <Typography variant="h1" style={{textAlign: 'center'}} className={csscommon.s_ttl01}>My <span className={csscommon.is_orange}>Crystal</span></Typography>
       </Stack>
       <Stack spacing={40} sx={{ mb: 5, position: "relative" }}>
         <Stack spacing={3} sx={{ mb: 1, position: "relative" }}>
           <Button
             variant="contained"
-            style={{ textAlign: "center", backgroundColor: "orange" }}
+            className={csscommon.c_btgg}
           >
             Sign in with Google
           </Button>
           <Button
             variant="contained"
-            style={{ textAlign: "center", backgroundColor: "orange" }}
             onClick={() => (window.location.href = "/auth/login")}
+            className={csscommon.c_btmail}
           >
-            <img src={SignInIcon} style={{marginRight: "0.5rem"}}></img>
             Sign in with Email
           </Button>
         </Stack>
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          gap="0.5rem"
-        >
-          <img src={SignUpIcon}></img>
-          <Typography onClick={() => (window.location.href = "/auth/signup")}>
-            Create new account
-          </Typography>
-        </Box>
+        <div className={csscommon.c_btsignup}>
+          <a className={csscommon.c_btsignup_link} href="/auth/signup/">
+            <span className={csscommon.c_btsignup_text}><i className={csscommon.arr}></i>Create new account</span>
+          </a>
+
+        </div>
       </Stack>
     </>
   );
